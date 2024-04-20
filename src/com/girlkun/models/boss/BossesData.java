fdbcbd72675bfd621e73a9056901b19750ff162f
Thi Do Thai <ngocthiazaaaa12@gmail.com>
2024-04-20 07:47:01 +0700

    update

--- src/com/girlkun/models/boss/BossesData.java
+++ src/com/girlkun/models/boss/BossesData.java
@@ -1882,10 +1882,10 @@ public class BossesData {
             new int[]{180}, //map join
             new int[][]{
                 {Skill.THOI_MIEN, 7, 100000},
-                // {Skill.KHIEN_NANG_LUONG, 7, 100000},
+                {Skill.KHIEN_NANG_LUONG, 7, 100000},
                 {Skill.KAMEJOKO, 7, 500},
                 {Skill.MASENKO, 7, 500},
-                // {Skill.TAI_TAO_NANG_LUONG, 7, 100000},
+                {Skill.TAI_TAO_NANG_LUONG, 1, 100000},
                 {Skill.DE_TRUNG, 7, 100000},
                 {Skill.THOI_MIEN, 7, 100000},
                 {Skill.THAI_DUONG_HA_SAN, 7, 100000},},//skill//skill
